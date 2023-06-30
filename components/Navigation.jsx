9915994b64b6f2d15d4ccc385c5e0edb688fde2f
Lekapo <lekapo@gmail.com>
2023-06-29 22:02:08 -0300

    changed colors, added active to links, removed classes

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,3 +1,4 @@
+"use client";
 /**
 Renders a navigation component with a sticky header, containing a logo and a link to other pages.
 @component
@@ -5,28 +6,51 @@ Renders a navigation component with a sticky header, containing a logo and a lin
 */
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+
 
 export const Navigation = () => {
+  
+{/* Get the current route */}
+const currentRoute = usePathname();
+
   return (
-    <div  className="m-auto w-full sticky top-0 border-black border-b-2 z-50 bg-white">
+    <div  className="sticky top-0 border-black border-b-4 z-50 bg-white">
       <nav  className="container mx-auto flex">
         <div id='navbrand' className="navbrand p-5 m-auto">
           <Link href="/">
-            <h1 className='text-lg font-extrabold uppercase'>Lekapo</h1>
+            <h1 className='text-2xl font-extrabold uppercase '>Lekapo</h1>
           </Link>
         </div>
         
         <div id='navbar' className="m-auto p-5 flex">
-        <Link href="/">
-              <h1 className='font-bold pr-5'>Portfolio</h1>
-          </Link>
           <Link href="/">
-              <h1 className='font-bold pr-5'>About</h1>
+            <button className={`${currentRoute === "/" 
+            ? "active" 
+            : "non-active-class-name"} 
+            text-black bg-black mr-5`}>
+              <div className="button-bg bg-white"></div>
+              <span className="text">Portfolio</span>
+            </button>
           </Link>
-          <Link href="/">
-              <h1 className='font-bold'>Contact</h1>
+          <Link href="/about">
+              <button className={`${currentRoute === "/about" 
+            ? "active" 
+            : "non-active-class-name"} 
+            text-black bg-black mr-5`}>
+                <div className="button-bg bg-white"></div>
+                <span className="text">About</span>
+              </button>
           </Link>
-          
+          <Link href="/contact">
+            <button className={`${currentRoute === "/contact" 
+            ? "active" 
+            : "non-active-class-name"} 
+            text-black bg-black mr-5`}>
+                <div className="button-bg bg-white"></div>
+                <span className="text">Contact</span>
+              </button>
+          </Link> 
         </div>
       </nav>
       
